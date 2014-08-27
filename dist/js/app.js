6c513c6979a131ee82b7263ca287c90eacb0f103
jhta <jeisonhs93@gmail.com>
2014-08-27 13:37:41 +0000

    vision de variables

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,5 +2,27 @@ $(document).ready(function(){
     $('#rootwizard').bootstrapWizard();
     $("#valor").rating();
     $("#valorA").rating();
+    
+    function limpiar(){
+        $("#formEspecifica").fadeOut("fast");
+        $("#formUniforme").fadeOut("fast");
+        $("#formCategorica").fadeOut("fast");
+    }
+    $("#varEspecifica").click(function(){
+        limpiar();
+        console.log("var especifca");
+        $("#formEspecifica").fadeIn();
+    });
+    
+    $("#varUniforme").click(function(){
+        limpiar();
+        console.log("var uniforme");
+        $("#formUniforme").fadeIn();
+    });
+    $("#varCategorica").click(function(){
+        limpiar();
+        console.log("var categorica");
+        $("#formCategorica").fadeIn();
+    });
     //
 });
