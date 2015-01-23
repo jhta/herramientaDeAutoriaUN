68a258fd7a6af83a7ad2eb1b58b9bf05de2d8974
jhta <jeisonhs93@gmail.com>
2015-01-23 14:23:25 -0500

    cambios base

--- dist/js/writetextbox.js
+++ dist/js/writetextbox.js
@@ -2,36 +2,65 @@
 $( document ).ready(function() {
 
     $("#div-iner").on("click", ".spa", function () {
-    if($(this).children().length < 1) {
-    $(this).html('<input id="input-new" type="text" style="padding: 10px 10px 10px 10px">');
-    $("#input-new").focus();
-    }
+            if($(this).children().length < 1) {
+                $(this).html('<input id="input-new" type="text" style="padding: 10px 10px 10px 10px">');
+                $("#input-new").focus();
+        }
     });
     $("#div-iner").on("blur", "#input-new", function () {
-    if (!$.trim(this.value).length) {
-    $(this).parent().html("");
-    }else{
-
-
-
-    if ($.trim(this.value).length>20) {
-    $(this).parent().html("");
-    }else{
-    addnewinput(this,"text");
-    /*
-     if(/^[0-9]+$/.test(this.value)){
-     addnewinput(this,"cons");
-     }else if(/^[a-zA-Z]+$/.test(this.value)){
-     addnewinput(this,"var");
-     }else{
-     $(this).parent().html("");
-     }*/
-    }
-    }
+        if (!$.trim(this.value).length) {
+            $(this).parent().html("");
+        }else{
+            if ($.trim(this.value).length>20) {
+            $(this).parent().html("");
+            }else{
+            addnewinput(this,"text");
+            /*
+             if(/^[0-9]+$/.test(this.value)){
+             addnewinput(this,"cons");
+             }else if(/^[a-zA-Z]+$/.test(this.value)){
+             addnewinput(this,"var");
+             }else{
+             $(this).parent().html("");
+             }*/
+            }
+        }
     });
     $("#div-iner").on("click", "#input-new", function (event) {
-    event.stopImmediatePropagation();
+        event.stopImmediatePropagation();
     });
 
+
+////////////////////////////////////////////////////////////////////////
+
+    $("#div-iner2").on("click", ".spa", function () {
+        alert("hola :D");
+        if($(this).children().length < 1) {
+            $(this).html('<input id="input-new" type="text" style="padding: 10px 10px 10px 10px">');
+            $("#input-new").focus();
+        }
     });
+    $("#div-iner2").on("blur", "#input-new", function () {
+        if (!$.trim(this.value).length) {
+            $(this).parent().html("");
+        }else{
+            if ($.trim(this.value).length>20) {
+            $(this).parent().html("");
+            }else{
+            addnewinput(this,"text");
+            /*
+             if(/^[0-9]+$/.test(this.value)){
+             addnewinput(this,"cons");
+             }else if(/^[a-zA-Z]+$/.test(this.value)){
+             addnewinput(this,"var");
+             }else{
+             $(this).parent().html("");
+             }*/
+            }
+        }
+    });
+    $("#div-iner2").on("click", "#input-new", function (event) {
+        event.stopImmediatePropagation();
+    });
+});
 
