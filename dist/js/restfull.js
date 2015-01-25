c78983cffa70eb4f889b4c514717cef05510a170
jonathanvm13 <jonathanvm13@gmail.com>
2015-01-25 12:12:58 -0500

    Integración de front con el back

--- dist/js/restfull.js
+++ dist/js/restfull.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
-
-    var client = new $.RestClient('http://localhost:4000/api/', {
-        username: 'admin',
-        password: 'secr3t'
-    });
-    client.add('user');
-    client.user.add('login');
+    var client;
 
     //Click en el botón Login
     $("#inputLogin").click(function(){
         var user = $("#username").val();
         var pass = $("#password").val();
 
+        client = new $.RestClient('http://localhost:4000/api/', {
+            username: user,
+            password: pass
+        });
+        client.add('user');
+        client.add('folder');
+        client.user.add('login');
+
+
         client.user.create({email:user,pass:pass},'login').done(function(data) {
-            console.log(data);
+            console.log(data.id);
+            sessionStorage.setItem('id', data.id);
+            sessionStorage.setItem('name', data.name);
+            $(location).attr('href','index.html');
         }).fail(function() {
-            alert( "error" );
+            alert( "Usuario o contraseña incorrecto" );
         })
     });
+
+
+
+
 
 });
