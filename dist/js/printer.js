c9879db567a67136db7c4d8eab1299258a9d7c9f
jhta <jeisonhs93@gmail.com>
2015-03-20 04:11:04 -0500

    se ve bien y ordenado la lista de errores

--- dist/js/printer.js
+++ dist/js/printer.js
@@ -53,32 +53,39 @@ var Printer = {
 	        "</div>" +
 	        "</div>" +
 	        "</div>");
-	    //$("#content-" + idRespuesta).html('<div style="border-style: solid; border-width: 1px;  font-family:inherit;font-size:inherit;font-weight:inherit;background:#ccc; border:1px solid #888;padding: 2px 4px;display:inline-block;"  data-id="'+idRespuesta+'" id="mathjax-'+idRespuesta+'"><math></math></div>');
 	},
 
 	htmlError: function( error, idRes ) {
-		 $("#"+idRes).find(".list-group").append(" <li class='list-group-item' id='"+error.id+"' data-respuestaid='"+idRes+"'>"+
-	        "<span class='glyphicon glyphicon-remove'></span>"+
-	        " <span>Error genuino:</span>"+
-	        "<div class='pull-right col-xs-6' style='display:table;'>"+
-	            "<div id='content-"+error.id+"' style='display: table-cell' class='col-xs-8'></div>"+
-	            "<div class='btn-toolbar col-xs-4  pull-right' style='display: table-cell' role='toolbar' aria-label='...'>"+
-	                "<div class='btn-group' role='group' aria-label='...'>"+
-	                    "<a href='#' data-id='"+error.id+"' class='pre-equation-respuesta' data-tipo='error' data-respuestaid='"+idRes+"'>"+
-	                        "<span class='glyphicon glyphicon-wrench'  aria-hidden='true' aria-hidden='true'></span>"+
-	                    "</a>"+
-	                "</div>"+
-	            "<div class='btn-group' role='group' aria-label='...'>"+
-                    "<a href='#' class='deleteErrorGenuino' data-id='"+error.id+"'  data-respuestaid='"+idRes+"'>"+
-                        "<span class='glyphicon glyphicon-remove'   aria-hidden='true'></span>"+
-                    " </a>"+
-	            "</div>"+
-                "<div class='btn-group' role='group' aria-label='...'>"+
-                    "<a href='#' class='deleteErrorGenuino' data-id='"+error.id+"'  data-respuestaid='"+idRes+"'>"+
-                        "<span class='glyphicon glyphicon-plus'   aria-hidden='true'></span>"+
-                     " </a>"+
+        console.debug("este es el motherfucker error que esta llegando: ", error);
+		 $("#"+idRes).find(".list-group").append(" <li class='list-group-item genuine-error' id='"+error.id+"' data-respuestaid='"+idRes+"'>"+
+	        "<div>"+
+                "<span class='glyphicon glyphicon-remove'></span>"+
+    	        " <span>Error genuino:</span>"+
+            "</div>"+
+	        "<div class='genuine-error__body'>"+
+	            "<div id='content-"+error.id+"' class='genuine-error__body-item input'>"+
+
+                    "<p class='label-res' id='p-error-"+error.id+"'>"+ error.formula +"</p>"+
+                    "<input type='text' class='hide form-control' id='text-"+error.id+"'>"+
+                    "<input type='text' class='hide input-res form-control' data-respuesta='" + idRes + "' data-error='" + error.id + "' id='error-"+error.id+"'>"+
+
+
                 "</div>"+
-	        "</div>"+
+	            "<div class='btn-toolbar  genuine-error__body-item'  role='toolbar' aria-label='...'>"+
+
+                    "<div class='btn-group' role='group' aria-label='...'>"+
+                        "<a href='#' data-id='"+error.id+"' data-respuestaid='"+idRes+"' class=' pre-equation-respuesta' data-tipo='correcta'>"+
+                        "<span class='glyphicon glyphicon-wrench' aria-hidden='true'></span>"+
+                        "</a>"+
+                    "</div>"+
+
+    	            "<div class='btn-group' role='group' aria-label='...'>"+
+                        "<a href='#' class='deleteErrorGenuino' data-id='"+error.id+"'  data-respuestaid='"+idRes+"'>"+
+                            "<span class='glyphicon glyphicon-remove'   aria-hidden='true'></span>"+
+                        " </a>"+
+    	            "</div>"+
+                    
+	           "</div>"+
 	        "</div>"+
 	        "</li>");
 
@@ -136,45 +143,32 @@ var Printer = {
 	},
 
 	addErrorToAnswer: function( data, error, respactual ) {
-		
-		$("#"+data).find(".list-group").append(
-            " <li class='list-group-item ' style='display: flex' id='"+error.id+"' data-respuestaid='"+respactual.id+"'>"+
-                "<div class='col-xs-6 p-0'>"+
+        $("#"+data).find(".list-group").append(
+            " <li class='list-group-item genuine-error' id='"+error.id+"' data-respuestaid='"+respactual.id+"'>"+
+                "<div class=' p-0'>"+
                     "<span class='glyphicon glyphicon-remove'></span>"+
                     " <span> Error genuino:</span>"+
                 "</div>"+
-                "<div class='pull-right col-xs-6 p-0' style='display:table;'>"+
-                    "<div  style='display: table-cell' class='input-space p-0 col-xs-10'>"+
+                "<div class='genuine-error__body'>"+
+                    "<div class='genuine-error__body-item input'>"+
                         "<p class='label-res hide' id='p-error-"+error.id+"'></p>"+
                         "<input type='text' class='hide form-control' id='text-"+error.id+"'>"+
                         "<input type='text' class='hide input-res form-control' data-respuesta='" + respactual.id + "' data-error='" + error.id + "' id='error-"+error.id+"'>"+
                     "</div>"+
-                "<div class='btn-toolbar col-xs-2 p-0 pull-right' style='display: table-cell' role='toolbar' aria-label='...'>"+
+                "</div>"+
+                "<div class='btn-toolbar genuine-error__body-item'  role='toolbar' aria-label='...'>"+
 
-                "<div class='btn-group' role='group' aria-label='...'>"+
-                    "<a href='#' data-id='"+error.id+"' data-respuestaid='"+respactual.id+"' class=' pre-equation-respuesta' data-tipo='correcta'>"+
-                    "<span class='glyphicon glyphicon-wrench' aria-hidden='true'></span>"+
-                    "</a>"+
-                "</div>"+
+                    "<div class='btn-group' role='group' aria-label='...'>"+
+                        "<a href='#' data-id='"+error.id+"' data-respuestaid='"+respactual.id+"' class=' pre-equation-respuesta' data-tipo='correcta'>"+
+                        "<span class='glyphicon glyphicon-wrench' aria-hidden='true'></span>"+
+                        "</a>"+
+                    "</div>"+
 
-                "<div class='btn-group' role='group' aria-label='...'>"+
-                    "<a href='#' class='deleteErrorGenuino' data-id='"+error.id+"'  data-respuestaid='"+respactual.id+"'>"+
-                        "<span class='glyphicon glyphicon-remove'   aria-hidden='true'></span>"+
-                    " </a>"+
-                "</div>"+
-                "<div class='btn-group' role='group' aria-label='...'>"+
-                    "<a href='#'  data-id='"+error.id+"'  data-respuestaid='"+respactual.id+"'>"+
-                        "<span class='glyphicon glyphicon-plus'   aria-hidden='true'></span>"+
-                    " </a>"+
-                "</div>"+
-
-                "<div class='btn-group' role='group' aria-label='...'>"+
-                    "<a href='#'  data-id='text-"+ error.id+"'  data-respuestaid='"+respactual.id+"'>"+
-                        "<span class='glyphicon glyphicon-wrench'   aria-hidden='true'></span>"+
-                    " </a>"+
-                "</div>"+
-
-                "</div>"+
+                    "<div class='btn-group' role='group' aria-label='...'>"+
+                        "<a href='#' class='deleteErrorGenuino' data-id='"+error.id+"'  data-respuestaid='"+respactual.id+"'>"+
+                            "<span class='glyphicon glyphicon-remove'   aria-hidden='true'></span>"+
+                        " </a>"+
+                    "</div>"+
                 "</div>"+
             "</li>");
 
